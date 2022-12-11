1b2b3db3ebbccf2ce8215b5a049f17849e444f53
covarom <huudoan.qna@gmail.com>
2022-12-11 15:09:10 +0700

    $$$$$$$

--- frontEnd/src/app/app.component.ts
+++ frontEnd/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core'
 import {Observable,of} from 'rxjs';
-
+import { coffee } from './cofee';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +11,21 @@ import {Observable,of} from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   constructor(private http:HttpClient) { }
   title = 'frontEnd';
+  datas:coffee[]=[];
+  getApi():Observable<coffee>{
+    return this.http.get<coffee>('/test/list').pipe(
+      );
+}
+ngOnInit(): void {
+  this.getCoffe();
+}
+  getCoffe(){
+    this.getApi().subscribe((res:any) => {
+      this.datas=res
+    })
+  }
 }
---
+++ frontEnd/src/app/cofee.ts
@@ -0,0 +1,7 @@
+export interface coffee {
+    title: string;
+    description: string;
+    ingredients: string;
+    id: number;
+    image:string
+  }
