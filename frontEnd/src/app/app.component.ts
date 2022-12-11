b53173bcae3887b4c25a361f7b2611fb49631990
covarom <huudoan.qna@gmail.com>
2022-12-11 10:29:51 +0700

    $$$$$

---
+++ frontEnd/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core'
+import {Observable,of} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  constructor(private http:HttpClient) { }
+  title = 'frontEnd';
+}
